c361812ac42a3bea35fbe4110985aed0f3ff4204
Keya Ochieng <keya.ochieng@strathmore.edu>
2025-09-06 11:53:08 +0300

    feat: Build multi-step checkout form UI and logic

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import {z} from "zod";
+
 export type ProductType = {
     id: string | number;
     name: string;
@@ -17,3 +19,30 @@ export type CartItemType = ProductType & {
 }
 
 export type CartItemsType = CartItemType[]
+
+//react hook shipping form schema using zod
+export const shippingFormSchema = z.object({
+    name: z.string().min(1, "Name is required!"),
+    email: z.email().min(1, "Email is required!"),
+    phone: z
+    .string()
+    .min(10,"Phone number must be 10 digits!")
+    .max(10,"Phone number must be 10 digits!")
+    .regex(/^\d+$/, "Phone number must contain only digits!"),
+    address: z.string().min(1,"Address is required!"),
+
+});
+
+export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+
+
+// Zod schema for the M-Pesa payment form
+export const paymentFormSchema = z.object({
+    mpesaPhone: z
+      .string()
+      .min(1, "M-Pesa phone number is required!") // Ensure it's not empty
+      .regex(/^(254\d{9})$/, "Phone number must be in the format 254xxxxxxxxx"), // Strict format for Kenya
+  });
+  
+  // The inferred type from the schema
+  export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
